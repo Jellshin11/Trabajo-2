9bf13fea8c42545cf15238446bc999d8b02801de
Jellshin11 <37632061+Jellshin11@users.noreply.github.com>
2018-11-19 21:42:26 -0600

    fun

--- lib/fun_with_strings.rb
+++ lib/fun_with_strings.rb
@@ -1,13 +1,50 @@
 module FunWithStrings
   def palindrome?
-    # your code here
+    init=self
+    ca1 = init.reverse.downcase
+    ca2 = init.downcase
+
+    if ca1 == ca2
+      a = true
+    else
+      a= false
+    end
+    return a
   end
-  def count_words
-    # your code here
+  def count_words(string)
+    fil = string.downcase.gsub(/[^a-zA-Z]/,"")
+    c= fil.split("")
+    cont = Hash.new
+    c.each { |x|
+      if(cont.has_key?(x))
+        cont[x] += 1;
+      else
+        cont[x] = 1;
+      end
+  }
+  return cont  
   end
   def anagram_groups
-    # your code here
+    hash = Hash.new
+    init = self
+    if init.empty?
+      return[]
+    end
+    s = init.split(/[^\w]/)
+    s.each do |x|
+      sort = x.split("").sort.join
+      if hash[sort] == nil
+        hash[sort] = [x]
+      else
+        hash[sort] << x
+      end
   end
+  result = []
+  hash.each_value do |y|
+    result << y
+  end
+  return result
+end
 end
 
 # make all the above functions available as instance methods on Strings:
