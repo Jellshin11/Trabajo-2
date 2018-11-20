e9c3afac34716965f431042619eedd3e86b00587
Jellshin11 <37632061+Jellshin11@users.noreply.github.com>
2018-11-19 21:55:41 -0600

    piedra

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -4,11 +4,42 @@ class RockPaperScissors
   class NoSuchStrategyError < StandardError ; end
 
   def self.winner(player1, player2)
-    # YOUR CODE HERE
+    raise NoSuchStrategyError, "Strategy must be one of R,P,S" unless player1[1] =~ /[SRP]/ && player2[1] =~ /[SRP]/
+   if player1[1]==player2[1]
+    	return player1
+  	end
+  	if player1[1] == "S" && player2[1] == "P"
+    	return player1
+  	end
+  	if player1[1]=="S" && player2[1]=="R"
+    	return player2
+  	end
+  	if player1[1]=="R" && player2[1]=="P"
+    	return player2
+  	end
+  	if player1[1]=="R" && player2[1]=="S"
+    	return player1
+  	end
+  	if player1[1]=="P" && player2[1]=="S"
+    	return player2
+  	end
+  	if player1[1]=="P" && player2[1]=="R"
+    	return player1
+  	end
   end
-
+ def self.is_player_move?(arr)
+  	return arr[0].is_a?(String) && arr[1].is_a?(String)
+ end
   def self.tournament_winner(tournament)
-    # YOUR CODE HERE
+    return get_winner(tournament[0], tournament[1])
+  end
+  def self.get_winner(a, b)
+  	raise WrongNumberOfPlayersError unless a.length == 2 && b.length == 2
+  	if is_player_move?(a) && is_player_move?(b)
+  		return winner(a, b)
+  	else
+  		return get_winner(get_winner(a[0], a[1]), get_winner(b[0], b[1]))
+  	end
   end
 
-end
+end 
